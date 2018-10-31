96789b2e20d93377e4e3b4278ef84f9f235369f1
Raphael Giron <maingiron@gmail.com>
2018-10-30 21:03:12 -0300

    entendendo o global

---
+++ exercicios-js/node/global.js
@@ -0,0 +1,7 @@
+// console.log(global)
+global.MinhaApp = Object.freeze({
+    saudacao() {
+        return 'Alterei o global, hehe'
+    },
+    nome: 'Sistema Legal'
+})
---
+++ exercicios-js/node/globalCliente.js
@@ -0,0 +1,6 @@
+require('./global')
+
+console.log(MinhaApp.saudacao())
+
+MinhaApp.nome = 'Eita, mudei o nome'
+console.log(MinhaApp.nome)
