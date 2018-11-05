4ce5cd449dcc8b88bdbb6bd9d1d7b94ce7e48135
Raphael Giron <maingiron@gmail.com>
2018-11-05 21:27:34 -0200

    revisão do esnext

---
+++ exercicios-js/esnext/revisao2.js
@@ -0,0 +1,24 @@
+// Arrow function
+const soma = (a, b) => a + b
+console.log(soma(2, 3))
+
+// Arrow function (this)
+const lexico1 = () => console.log(this === module.exports)
+const lexico2 = lexico1.bind({})
+lexico1()
+lexico2()
+
+// Parametro default
+function log(texto = 'Node') {
+    console.log(texto)
+}
+log()
+log('Mudei...')
+
+// Operador rest
+function total(...numeros) {
+    let total = 0
+    numeros.forEach(n => total += n)
+    return total
+}
+console.log(total(2, 3, 4, 5))
