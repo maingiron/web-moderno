6295b2c5178322de06ae762c7ae1308a67cbd28b
Raphael Giron <maingiron@gmail.com>
2019-01-03 22:43:41 -0200

    xhr2

--- exercicios-web/ajax/server.js
+++ exercicios-web/ajax/server.js
@@ -3,7 +3,30 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocurreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
 
 app.listen(8080, () => console.log('Executando...'))
