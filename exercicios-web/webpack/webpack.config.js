cadf3de6c4918ad6f0343f01ee7f0d2f16f6fba1
Raphael Giron <maingiron@gmail.com>
2019-01-18 21:18:04 -0200

    criado script para development e prodution

--- exercicios-web/webpack/webpack.config.js
+++ exercicios-web/webpack/webpack.config.js
@@ -1,12 +1,25 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const uglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const optimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new uglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new optimizeCssAssetsPlugin({})
+        ]
     },
     plugins: [
         new miniCssExtractPlugin({
