12f371040919b67217bc3131511b5764182d8fdd
Raphael Giron <maingiron@gmail.com>
2019-01-29 23:06:22 -0200

    finalizado projeto calculadora

--- exercicios-web/vue/calculator/src/main/Calculator.vue
+++ exercicios-web/vue/calculator/src/main/Calculator.vue
@@ -26,16 +26,54 @@ import Display from "../components/Display"
 import Button from "../components/Button"
 
 export default {
+    data: function() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0
+        }
+    },
     components: { Display, Button },
     methods: {
         clearMemory() {
-            
+            Object.assign(this.$data, this.$options.data())
         },
         setOperation(operation) {
-            
+            if (this.current === 0) {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                try {
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                } catch (e) {
+                    this.$emit('onError', e)
+                }
+
+                this.values[1] = 0
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
         },
         addDigit(n) {
-            
+            if (n === "." && this.displayValue.includes(".")) return
+
+            const clearDisplay = this.displayValue === "0" || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+
+            this.displayValue = displayValue
+            this.clearDisplay = false
+            this.values[this.current] = displayValue
         }
     }
 }
