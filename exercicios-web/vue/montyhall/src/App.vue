689f09c5dd8f41b123270b18d2eb5453a912debe
Raphael Giron <maingiron@gmail.com>
2019-01-30 18:16:04 -0200

    init project montyhall

---
+++ exercicios-web/vue/montyhall/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+        <h1>Problema de Monty Hall</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+body {
+    color: #FFF;
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+}
+#app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+#app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+}
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+.form, .form input, .form button {
+    margin-bottom: 10px;
+    font-size: 2rem;
+}
+.doors {
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+}
+</style>
