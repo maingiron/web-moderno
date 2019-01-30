afb2cf8fceed160ab179bd5ec1c72974f215350d
Raphael Giron <maingiron@gmail.com>
2019-01-30 18:55:44 -0200

    finished project monty hall

--- exercicios-web/vue/montyhall/src/App.vue
+++ exercicios-web/vue/montyhall/src/App.vue
@@ -1,7 +1,25 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
-        <Door number="1" :hasGift="false" />
+        <div class="form">
+            <div v-if="!started">
+                <label for="portsAmount">Quantas portas? </label>
+                <input type="text" id="portsAmount" size="3"
+                    v-model.number="portsAmount">
+            </div>
+            <div v-if="!started">
+                <label for="selectedPort">Qual porta é premiada? </label>
+                <input type="text" id="selectedPort" size="3"
+                    v-model.number="selectedPort">
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in portsAmount" :key="i">
+                <Door :hasGift="i === selectedPort" :number="i" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,7 +28,14 @@ import Door from './components/Door'
 
 export default {
     name: 'App',
-    components: { Door }
+    components: { Door },
+    data: function() {
+        return {
+            started: false,
+            portsAmount: 3,
+            selectedPort: null
+        }
+    }
 }
 </script>
 
