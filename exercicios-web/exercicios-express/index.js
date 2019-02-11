954f9fd8f1841286973548d4a1be1c8ab9c51896
Raphael Giron <maingiron@gmail.com>
2019-02-11 16:31:46 -0200

    add respost

--- exercicios-web/exercicios-express/index.js
+++ exercicios-web/exercicios-express/index.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
 
+app.get('/opa', (req, res) => {
+  res.json({
+    data: [
+      {id: 7, name: 'Ana', position: 1 },
+      {id: 34, name: 'Bia', position: 2 },
+      {id: 73, name: 'Carlos', position: 3 }
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200
+  })
+  // res.json({
+  //   nome: "Raphel Giron",
+  //   profissao: "Analista de Sistema"
+  // })
+  // res.send('Estou bem')
+})
+
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000')
 })
