d9f446c09916e1563209077dd0e55be8f31492e7
Raphael Giron <maingiron@gmail.com>
2019-02-22 21:43:22 -0300

    created header components

--- exercicios-web/react/crud/frontend/src/main/App.jsx
+++ exercicios-web/react/crud/frontend/src/main/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
@@ -9,6 +11,6 @@ export default props =>
   <div className="app">
     <Logo />
     <Nav />
-    <Main />
+    <Main icon="home" title="Início" subtitle="Segunda projeto do capítulo de React." />
     <Footer />
   </div>
