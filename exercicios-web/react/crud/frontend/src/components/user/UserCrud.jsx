57dba543a259f9df51925a51cf4bcaa3e08d75b8
Raphael Giron <maingiron@gmail.com>
2019-02-24 11:10:32 -0300

    add crud user component

---
+++ exercicios-web/react/crud/frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import Main from '../template/Main'
+
+const headerProps = {
+  icon: 'users',
+  title: 'Usuários',
+  subtitle: 'Cadastro de usuário: Incluir, Listar, Alterar e Excluir'
+}
+
+export default class UserCrud extends Component {
+  render() {
+    return (
+      <Main {...headerProps}>
+        Crud Usuários
+      </Main>
+    )
+  }
+}
