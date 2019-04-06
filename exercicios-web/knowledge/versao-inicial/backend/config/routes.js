73ddbe4a6dcbd10055076f480fc30294b219d82c
Raphael Giron <maingiron@gmail.com>
2019-04-06 17:05:46 -0300

    created api category

--- exercicios-web/knowledge/versao-inicial/backend/config/routes.js
+++ exercicios-web/knowledge/versao-inicial/backend/config/routes.js
@@ -6,4 +6,13 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById)
+
+    app.route('/categories')
+        .post(app.api.category.save)
+        .get(app.api.category.get)
+
+    app.route('/categories/:id')
+        .put(app.api.category.save)
+        .get(app.api.category.getById)
+        .delete(app.api.category.remove)
 }
