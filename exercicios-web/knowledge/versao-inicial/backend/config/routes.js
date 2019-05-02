af0ea1b9d8eb775b69953c04c178dab5e4cd927e
Raphael Giron <maingiron@gmail.com>
2019-05-02 19:26:53 -0300

    api articles

--- exercicios-web/knowledge/versao-inicial/backend/config/routes.js
+++ exercicios-web/knowledge/versao-inicial/backend/config/routes.js
@@ -19,4 +19,16 @@ module.exports = app => {
         .put(app.api.category.save)
         .get(app.api.category.getById)
         .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 }
