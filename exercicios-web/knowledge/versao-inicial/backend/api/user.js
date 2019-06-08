f8df10aaf3c82e643216acdc818bdcb7769ef6ca
Raphael Giron <maingiron@gmail.com>
2019-06-08 16:49:25 -0300

    add validation user admin

--- exercicios-web/knowledge/versao-inicial/backend/api/user.js
+++ exercicios-web/knowledge/versao-inicial/backend/api/user.js
@@ -13,6 +13,9 @@ module.exports = app => {
 
         if (req.params.id) user.id = req.params.id
 
+        if (!req.originalUrl.startsWith('/users')) user.admin = false
+        if (!req.user || !req.user.admin) user.admin = false
+
         try {
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'E-mail não informado')
