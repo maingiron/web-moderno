1bb78f8b4e544f9bd73801b52193264eefebd25d
Raphael Giron <maingiron@gmail.com>
2019-06-13 20:54:32 -0300

    add template

---
+++ exercicios-web/knowledge/versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+        <span>Copyright <strong>2015-2019</strong></span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
