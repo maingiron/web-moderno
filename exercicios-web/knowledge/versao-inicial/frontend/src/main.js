122cfb6138b0b2438636fbd7fcdb149b737fa4e5
Raphael Giron <maingiron@gmail.com>
2019-06-14 20:49:10 -0300

    add home component

--- exercicios-web/knowledge/versao-inicial/frontend/src/main.js
+++ exercicios-web/knowledge/versao-inicial/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from "@/config/router"
 
 Vue.config.productionTip = false
 
+// Temp
+require("axios").defaults.headers.common["Authorization"] = "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkFuYSIsImVtYWlsIjoiYW5hQGdtYWlsLmNvbSIsImFkbWluIjpmYWxzZSwiaWF0IjoxNTYwNTU1NTM1LCJleHAiOjE1NjA4MTQ3MzV9.3pppHNubr8u8X67mHrX97Cq5BnPU4UF8LmDv8Oid2hc"
+
 new Vue({
   store,
   router,
