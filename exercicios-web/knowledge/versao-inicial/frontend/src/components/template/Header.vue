f743cc4736b59c85eb92586adddc1a31ec32a3dd
Raphael Giron <maingiron@gmail.com>
2019-06-13 22:10:28 -0300

    add user dropdown

--- exercicios-web/knowledge/versao-inicial/frontend/src/components/template/Header.vue
+++ exercicios-web/knowledge/versao-inicial/frontend/src/components/template/Header.vue
@@ -6,15 +6,20 @@
         <h1 class="title">
             {{ title }}
         </h1>
+        <UserDropdown v-if="!hideUserDropdown" />
     </header>
 </template>
 
 <script>
+import UserDropdown from './UserDropdown'
+
 export default {
     name: 'Header',
+    components: { UserDropdown },
     props: {
         title: String,
-        hideToggle: Boolean
+        hideToggle: Boolean,
+        hideUserDropdown: Boolean
     },
     computed: {
         icon() {
@@ -64,5 +69,7 @@ export default {
 
     header.header > a.toggle:hover {
         background-color: rgba(0, 0, 0, 0.2);
+        color: #fff;
+        cursor: pointer;
     }
 </style>
