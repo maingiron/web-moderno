ed122b631e1e51fa1faadd8a36f943d32a4ae1ff
Raphael Giron <maingiron@gmail.com>
2019-06-14 21:17:05 -0300

    add vue toasted

--- exercicios-web/knowledge/versao-inicial/frontend/src/main.js
+++ exercicios-web/knowledge/versao-inicial/frontend/src/main.js
@@ -1,10 +1,11 @@
-import 'font-awesome/css/font-awesome.css'
-import Vue from 'vue'
+import "font-awesome/css/font-awesome.css"
+import Vue from "vue"
 
-import App from './App'
+import App from "./App"
 
-import '@/config/bootstrap'
-import store from '@/config/store'
+import "@/config/bootstrap"
+import "@/config/msgs"
+import store from "@/config/store"
 import router from "@/config/router"
 
 Vue.config.productionTip = false
@@ -16,4 +17,4 @@ new Vue({
   store,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
