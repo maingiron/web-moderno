4e7164edf500a97351e26988e6e3b1dc84431291
Raphael Giron <maingiron@gmail.com>
2019-06-17 21:50:08 -0300

    change menu component

--- exercicios-web/knowledge/versao-inicial/frontend/src/config/router.js
+++ exercicios-web/knowledge/versao-inicial/frontend/src/config/router.js
@@ -16,7 +16,7 @@ const routes = [{
     path: "/admin",
     component: AdminPages
 }, {
-    name: "articleByCategory",
+    name: "articlesByCategory",
     path: "/categories/:id/articles",
     component: ArticlesByCategory
 }, {
